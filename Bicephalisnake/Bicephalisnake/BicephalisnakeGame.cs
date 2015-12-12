80bed9b60ee976b1b1d3e30ce4ef4de581be6f7d
tphx <tlaceyphx@gmail.com>
2015-12-11 20:27:56 -0700

    Added game states.

--- Bicephalisnake/Bicephalisnake/BicephalisnakeGame.cs
+++ Bicephalisnake/Bicephalisnake/BicephalisnakeGame.cs
@@ -6,8 +6,16 @@ namespace Tphx.Bicephalisnake
 {
     public class Game1 : Game
     {
+        private enum GameState
+        {
+            Uninitialized,
+            Playing
+        };
+
+        private GameState gameState = GameState.Uninitialized;
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
+        private GameComponent activeComponent;
 
         public Game1()
         {
@@ -28,6 +36,8 @@ namespace Tphx.Bicephalisnake
         protected override void LoadContent()
         {
             this.spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ChangeGameState(GameState.Playing);
         }
 
         protected override void UnloadContent()
@@ -44,6 +54,26 @@ namespace Tphx.Bicephalisnake
             this.GraphicsDevice.Clear(Color.CornflowerBlue);
 
             base.Draw(gameTime);
+        }
+
+        private void ChangeGameState(GameState newGameState)
+        {
+            if(newGameState != this.gameState)
+            {
+                if(this.activeComponent != null)
+                {
+                    this.activeComponent.Dispose();
+                }
+
+                switch(newGameState)
+                {
+                    case GameState.Uninitialized:
+                        this.activeComponent = null;
+                        break;
+                    case GameState.Playing:
+                        break;
+                }
+            }
         }
     }
 }
